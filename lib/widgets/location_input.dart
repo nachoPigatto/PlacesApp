757de63c2e96a2a48c94e94fdf659f139110a2a3
Ignacio Pigatto <ignacio.pigatto@gmail.com>
2023-08-15 15:46:47 -0300

    PlacesApp

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  PlaceLocation? _pickedLocation;
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    if (lat == null || lng == null) {
+      return;
+    }
+
+    // final url = Uri.parse(
+    //     'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=YOUR_API_KEY');// change your api key with google maps api key
+    //final response = await http.get(url);
+    // final resData = json.decode(response.body);
+    // final address = resData['results'][0]['formatted_address'];
+
+    setState(() {
+      //_pickedLocation = PlaceLocation(latitude: lat, longitude: lng, address: address);
+      _isGettingLocation = false;
+    });
+  }
+  //video 250
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chose',
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+    );
+
+    if (_isGettingLocation = true) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          height: 170,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+            ),
+          ),
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: const Icon(Icons.location_on),
+              onPressed: _getCurrentLocation,
+              label: const Text('Get Current Location'),
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.map),
+              onPressed: () {},
+              label: const Text('Select on Map'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
